3d80841337097d7508bebc05a7b181e343fb9cf0
Mario Partida <partidam578@gmail.com>
2024-02-25 13:35:55 -0700

    Added POST route to read from json file and write a new note with a uuid

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const { json } = require("express");
 const fs = require("fs");
 
@@ -14,6 +15,39 @@ router.get("/notes", (req, res) => {
   });
 });
 
-router.post("/notes", (req, res) => {});
+router.post("/notes", async (req, res) => {
+  try {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const notes = JSON.parse(data);
+        const newNote = {
+          id: uuidv4(),
+          title: req.body.title,
+          text: req.body.text,
+        };
+        notes.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(notes, null, 2),
+          "utf8",
+          (err, data) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log(data);
+          }
+        );
+        res.status(201).json(newNote);
+      }
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// router.delete("/notes/:id", (req, res) => {});
 
 module.exports = router;
